d84d97bb7a34e06a9861c969ee01f45b34c17d03
robert-m <robix.m@gmail.com>
2018-05-24 16:43:47 +0200

    carousel + line-chart

--- src/app/carousel/carousel.component.ts
+++ src/app/carousel/carousel.component.ts
@@ -9,10 +9,10 @@ export class CarouselComponent implements OnInit {
   items: Array<string> = [];
   constructor() {
     this.items = [
-      'assets/images/1.jpg',
       'assets/images/2.PNG',
       'assets/images/3.PNG',
       'assets/images/4.PNG',
+      'assets/images/2.PNG',
     ];
   }
   public newCarouselItem: string;
